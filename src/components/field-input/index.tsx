d1567b653f4cb6d14575d59bf33fda54077075fd
lin-mt <lin-mt@outlook.com>
2022-10-09 23:17:08 +0800

    feat: field name can't empty.

--- src/components/field-input/index.tsx
+++ src/components/field-input/index.tsx
@@ -1,5 +1,6 @@
-import React, { ReactElement, ReactNode, useEffect, useState } from 'react';
-import { Input } from '@arco-design/web-react';
+import React, { ReactElement, ReactNode, useEffect, useRef, useState } from 'react';
+import { Input, Message } from '@arco-design/web-react';
+import { RefInputType } from '@arco-design/web-react/es/Input/interface';
 
 interface FieldInputProp {
   value: string;
@@ -9,18 +10,55 @@ interface FieldInputProp {
 
 const FieldInput = (props: FieldInputProp): ReactElement => {
   const [fieldValue, setFieldValue] = useState<string>(props.value);
+  const [error, setError] = useState<boolean>(false);
+  const [placeholder, setPlaceholder] = useState<string>();
+  const ref = useRef<RefInputType>(null);
 
   useEffect(() => {
     setFieldValue(props.value);
   }, [props.value]);
 
   const handleChange = (value) => {
+    if (value.length === 0) {
+      Message.warning('FieldName can not empty.');
+      setError(true);
+      setFieldValue((prevState) => {
+        setPlaceholder(prevState);
+        return value;
+      });
+      return;
+    }
+    if (placeholder === value) {
+      setFieldValue(value);
+      return;
+    }
+    setPlaceholder('');
+    setError(false);
     if (props.onChange(value)) {
       setFieldValue(value);
     }
   };
 
-  return <Input addAfter={props.addAfter} value={fieldValue} onChange={handleChange} />;
+  useEffect(() => {
+    ref.current.dom.addEventListener('focusout', () => {
+      if (ref.current.dom.value.length === 0) {
+        Message.warning('FieldName can not empty.');
+        ref.current.dom.focus();
+      }
+    });
+  }, []);
+
+  return (
+    <Input
+      autoFocus
+      ref={ref}
+      error={error}
+      addAfter={props.addAfter}
+      placeholder={placeholder}
+      value={fieldValue}
+      onChange={handleChange}
+    />
+  );
 };
 
 export default FieldInput;
